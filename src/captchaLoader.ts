192700f47ab77e780d1aaf3dc5fefeb5feb15466
Andrew Diego Beers <andrew.d.beers@gmail.com>
2023-06-20 19:41:07 -0400

    Add dotnet dependencies

---
+++ src/captchaLoader.ts
@@ -0,0 +1,15 @@
+import {DotNet} from  "@microsoft/dotnet-js-interop";
+
+class captchaLoader {
+    async loadAsync(siteKey: string, dotNetObjRef: DotNet.DotNetObject): Promise<void> {
+
+    }
+
+    async executeAsync(siteKey: string, dotNetObjRef: DotNet.DotNetObject): Promise<void> {
+
+    }
+
+    async renderAsync(siteKey: string, dotNetObjRef: DotNet.DotNetObject): Promise<void> {
+        
+    }
+}
